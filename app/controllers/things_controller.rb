a7cc8efa3b5c2b5c5c55b5cea1572ecccc707f5c
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-04-11 15:45:45 -0600

    implement low level caching for things

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,6 +1,7 @@
 class ThingsController < ApplicationController
   def index
-    render json: Thing.all,
+    @things = Rails.cache.fetch('things', expires_in: 5.minutes) {Thing.all}
+    render json: @things,
     each_serializer: ThingsSerializer
   end
 end
